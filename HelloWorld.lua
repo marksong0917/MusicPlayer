08d736fdd7a5ced0d67c7994eb2935d4b1fb273a
Ze Song <200395429@student.georgianc.on.ca>
2020-09-22 11:05:49 -0400

    final commit

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -2,15 +2,20 @@
 SLASH_HELLO1 = "/helloworld" 
 SLASH_HELLO2 = "/hellow" 
 
--- Greeting message 
-local function showGreeting(name)
-    local greeting = "Hello, " .. name .. "!" 
 
+-- Greeting message 
+
+local function showGreeting(name)
+    local worldPing = (select(4,GetNetStats())) 
+    local currentTime = ((select(1,GetGameTime())))  - 12  -- Coverting 24 hour format into 12
+    local greeting = "Hello, " .. name .. "!" .. " current time is: " .. currentTime ..
+    " Your current ping is " .. worldPing .. "  Thank You For Using Hello World  "
     message(greeting)
 
 end
 
--- /hello world name 
+-- /hello world name  
+
 local function HelloWorldHandler(name) 
     local nameExists = string.len(name) > 0 
     -- name 
@@ -24,6 +29,4 @@ local function HelloWorldHandler(name)
 end 
 
 SlashCmdList["HELLO"] = HelloWorldHandler; 
-
-
 
